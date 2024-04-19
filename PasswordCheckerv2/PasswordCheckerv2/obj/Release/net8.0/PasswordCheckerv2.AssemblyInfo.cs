49bb58eb7c440d35bc67edd000466611a8f923da
sztacha <sztachaprograming@gmail.com>
2024-04-19 15:17:37 +0200

    bbnvc

--- PasswordCheckerv2/PasswordCheckerv2/obj/Release/net8.0/PasswordCheckerv2.AssemblyInfo.cs
+++ PasswordCheckerv2/PasswordCheckerv2/obj/Release/net8.0/PasswordCheckerv2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordCheckerv2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+295899f7fc647629aaeeefc2af330f0a589685f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordCheckerv2")]
