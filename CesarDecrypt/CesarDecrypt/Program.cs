ab3132ca0b6168f6aa48c667f708820464d924e9
sztacha <sztachaprograming@gmail.com>
2024-05-08 21:00:50 +0200

    Your commit message

--- CesarDecrypt/CesarDecrypt/Program.cs
+++ CesarDecrypt/CesarDecrypt/Program.cs
@@ -12,7 +12,8 @@ namespace CaesarCipher
             // Asking the user to input a secret message to convert it into an encrypted one
 
             Console.WriteLine("Type a secret message and we will convert it for you into an encrypted message: ");
-            string userMessage = Console.ReadLine();
+            string userMessageToLower = Console.ReadLine();
+            string userMessage = userMessageToLower.ToLower();
 
             // Converting captured string from user to array of chaaracters
 
@@ -20,21 +21,21 @@ namespace CaesarCipher
 
             // New empty array of characters with length of "secretMessage"
 
-            char[] encryptedMessage =  new char[secretMessage.Length];
+            char[] encryptedMessage = new char[secretMessage.Length];
 
             // Creating loop For that loops through each character of "secretMessage"
 
-            for(int i = 0; i < secretMessage.Length; i++)
+            for (int i = 0; i < secretMessage.Length; i++)
             {
                 // Creating char that store position of char 'i' from "secretMessage"
                 // for example in "Hello world" secretMessage[0] is H
                 char letter = secretMessage[i];
 
-                // Searching for the index position of "letter" within the "alphabet" array
+                // Searching for the index position of "letter" within the "alphabet" array and store it
                 int charPosition = Array.IndexOf(alphabet, letter);
 
                 // Adding 3 positions to a letter
-                int cryptedLetter = charPosition + 3;
+                int cryptedLetter = (charPosition + 3) % 26;
 
                 // Creating new encrypted letter
                 char newEncryptedMessage = alphabet[cryptedLetter];
@@ -43,8 +44,9 @@ namespace CaesarCipher
                 encryptedMessage[i] = newEncryptedMessage;
                 
             }
-            
 
+            String.Join("", encryptedMessage);
+            Console.WriteLine(encryptedMessage);
 
         }
     }
