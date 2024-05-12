aa5b193287ddb911d31baafa9113ee121d15db55
sztacha <sztachaprograming@gmail.com>
2024-05-12 21:56:04 +0200

    Adding Quizgame

--- CesarDecrypt/CesarDecrypt/Program.cs
+++ CesarDecrypt/CesarDecrypt/Program.cs
@@ -1,20 +1,48 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace CaesarCipher
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static bool IsAllLetters(string s)
         {
+            bool containsLetter = false;
+            foreach (char c in s)
+            {
+                if (char.IsLetter(c))
+                {
+                    containsLetter = true;
+                }
+                else if (c != ' ')
+                {
+                    return false; // Znaleziono znak, który nie jest literą ani spacją
+                }
+            }
+
+            return containsLetter; // Jeśli przynajmniej jedna litera została znaleziona, zwróć true, w przeciwnym razie false
+
+        }
+        public static void Encrypt()
+        {
+            
             char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-
-            // Asking user to type a secret message to convert into crypted message
-            // Asking the user to input a secret message to convert it into an encrypted one
-
-            Console.WriteLine("Type a secret message and we will convert it for you into an encrypted message: ");
-            string userMessageToLower = Console.ReadLine();
-            string userMessage = userMessageToLower.ToLower();
-
+            Console.Write("What message you want to encrypt?: ");
+            string userMessage = Console.ReadLine().ToLower();
+            while (true)
+            {
+                if (IsAllLetters(userMessage))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Try letters");
+                    userMessage = Console.ReadLine().ToLower();
+                }
+            }
             // Converting captured string from user to array of chaaracters
 
             char[] secretMessage = userMessage.ToCharArray();
@@ -23,15 +51,13 @@ namespace CaesarCipher
 
             char[] encryptedMessage = new char[secretMessage.Length];
 
-            // Creating loop For that loops through each character of "secretMessage"
-
             for (int i = 0; i < secretMessage.Length; i++)
             {
                 // Creating char that store position of char 'i' from "secretMessage"
                 // for example in "Hello world" secretMessage[0] is H
                 char letter = secretMessage[i];
 
-                // Searching for the index position of "letter" within the "alphabet" array and store it
+                // Searching for the index position of "letter" within the "alphabet" array
                 int charPosition = Array.IndexOf(alphabet, letter);
 
                 // Adding 3 positions to a letter
@@ -42,11 +68,81 @@ namespace CaesarCipher
 
                 // Assigning the new encrypted letter to the message:
                 encryptedMessage[i] = newEncryptedMessage;
-                
+
             }
 
-            String.Join("", encryptedMessage);
-            Console.WriteLine(encryptedMessage);
+            string newwEncryptedMessage = String.Join("", encryptedMessage);
+            Console.WriteLine(newwEncryptedMessage);
+
+        }
+        public static void Decrypt()
+        {
+
+            char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            Console.Write("What message you want to encrypt?: ");
+            string userMessage = Console.ReadLine().ToLower();
+            while (true)
+            {
+                if (IsAllLetters(userMessage))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Try letters");
+                    userMessage = Console.ReadLine().ToLower();
+                }
+            }
+            // Converting captured string from user to array of chaaracters
+
+            char[] secretMessage = userMessage.ToCharArray();
+
+            // New empty array of characters with length of "secretMessage"
+
+            char[] encryptedMessage = new char[secretMessage.Length];
+
+            for (int i = 0; i < secretMessage.Length; i++)
+            {
+                // Creating char that store position of char 'i' from "secretMessage"
+                // for example in "Hello world" secretMessage[0] is H
+                char letter = secretMessage[i];
+
+                // Searching for the index position of "letter" within the "alphabet" array
+                int charPosition = Array.IndexOf(alphabet, letter);
+
+                // Adding 3 positions to a letter
+                int cryptedLetter = (charPosition - 3) % 26;
+
+                // Creating new encrypted letter
+                char newEncryptedMessage = alphabet[cryptedLetter];
+
+                // Assigning the new encrypted letter to the message:
+                encryptedMessage[i] = newEncryptedMessage;
+
+            }
+
+            string newwEncryptedMessage = String.Join("", encryptedMessage);
+            Console.WriteLine(newwEncryptedMessage);
+
+        }
+
+        static void Main(string[] args)
+        {
+            string userInput = "".ToLower();
+            Console.WriteLine("You wanna 1.Encrypt or 2.Decrypt a message?: ");
+            userInput = Console.ReadLine();
+
+            if (userInput == "1" || userInput == "encrypt")
+            {
+                Encrypt();
+            }
+            else if(userInput == "2" || userInput == "decrypt")
+            {
+                Decrypt(); 
+            }
+
+            //Console.WriteLine("Your encrypted message is: "); Encrypt();
+
 
         }
     }
---
+++ QuizGame/QuizGame/Program.cs
@@ -0,0 +1,93 @@
+﻿using Accord.Math;
+using System;
+using System.IO;
+
+namespace TrueOrFalse
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            // Do not edit these lines
+            Console.WriteLine("Welcome to 'True or False?'\nPress Enter to begin:");
+            string entry = Console.ReadLine();
+            Tools.SetUpInputStream(entry);
+
+            // Type your code below
+            //Creating array with questions
+            string[] questions = { "The biggest mammal is blue whale?", "capital of Poland is Coratia?", "Was the Polish baptism in 996??", "Leonardo de Caprio painted the Mona Lisa?"};
+
+            //Creating bool array with length of questions array
+            bool[] answers = new bool[]
+            {
+                true,
+                false,
+                false,
+                false,
+            };
+
+            //Creating bool array with reposness from user
+            bool[] responses = new bool[questions.Length];
+
+            //Creating if loop that checks if questions and answers are with the same length
+
+            if(answers.Length != questions.Length)
+            {
+                Console.WriteLine("You didnt answer for all questions");
+            }
+
+            int askingIndex = 0;
+
+            // Foreach loop that checks every question variable
+            foreach(string question in questions)
+            {
+                string input;
+                bool isBool;
+                bool inputBool;
+
+                Console.WriteLine(question);
+                Console.WriteLine("True of False?");
+
+                input = Console.ReadLine();
+
+                isBool = Boolean.TryParse(input, out inputBool);
+
+                while(!isBool)
+                {
+                    Console.WriteLine("Please respond with 'true' or 'false' ");
+                    input = Console.ReadLine();
+                    isBool = Boolean.TryParse(input, out inputBool);
+                }
+                responses[askingIndex++] = inputBool;
+                
+            }
+           foreach(bool response in responses)
+            {
+                Console.WriteLine(response);
+            }
+
+            //to loop through the responses
+            int scoringIndex = 0;
+            //to count the number of correct responses
+            int score = 0;
+
+            foreach(bool answer in answers)
+            {
+                bool responsIndexing = responses[scoringIndex++];
+                Console.WriteLine($"Input: {responsIndexing} || Answer: {answer}");
+                if(responsIndexing ==  answer)
+                {
+                    score++;
+                }
+                ;
+
+            }
+            Console.WriteLine($"You've got {score} of 4 correct");
+
+
+
+        }
+    }
+}
