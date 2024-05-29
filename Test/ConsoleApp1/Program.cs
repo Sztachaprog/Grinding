a98a84e237ca865163572eb0885b123f191f5499
sztacha <sztachaprograming@gmail.com>
2024-05-29 14:47:52 +0200

    Trying new features like inherites and override

---
+++ Test/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp1;
+
+class Program
+{
+    static void Main()
+    {
+        // Ebook initalization
+        Ebook ebook = new Ebook("dziady", "mickiewicz", 12, 12);
+
+        Console.WriteLine(ebook.GetDescription());
+
+
+        // Book initalization
+        Book book = new Book("dziady", "mickiewicz", 12);
+
+        Console.WriteLine(book.GetDescription());
+
+
+
+    }
+}
